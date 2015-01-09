997f9afa1965ed6433e033e8033776a525c5b0aa
Wen-Tien Chang <ihower@gmail.com>
2015-01-09 18:48:40 +0800

    Add group and event_groupship models

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,7 @@ class Event < ActiveRecord::Base
 
   has_many :comments
 
+  has_many :event_groupships
+  has_many :groups, :through => :event_groupships
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109102736) do
+ActiveRecord::Schema.define(version: 20150109104657) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20150109102736) do
   create_table "comments", force: :cascade do |t|
     t.text     "content"
     t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "event_groupships", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,6 +45,12 @@ ActiveRecord::Schema.define(version: 20150109102736) do
   end
 
   add_index "events", ["category_id"], name: "index_events_on_category_id"
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
