366f4a6138477689cae3fb1925deb5874c80df2e
Wen-Tien Chang <ihower@gmail.com>
2015-01-09 19:12:56 +0800

    Install devise

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
 
+  before_action :authenticate_user!
   before_action :set_event, :only => [ :show, :dashboard, :edit, :update, :destroy]
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :events do
     resources :comments, :controller => 'event_comments'
 
