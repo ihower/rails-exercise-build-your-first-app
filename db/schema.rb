dad4e706b31239f6f8b5a648219000508e683446
Wen-Tien Chang <ihower@gmail.com>
2015-01-09 18:17:37 +0800

    Add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109081007) do
+ActiveRecord::Schema.define(version: 20150109101257) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
