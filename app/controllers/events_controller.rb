12cfc42f9645dbefd7866105776e22dcdd454dd6
Wen-Tien Chang <ihower@gmail.com>
2015-01-09 16:50:28 +0800

    Add events new/create actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,21 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+
+    redirect_to :action => :index
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description)
+  end
+
 end
